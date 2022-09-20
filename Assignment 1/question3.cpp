2738f6c0e41ff134364f9c67ac8b502d7fc8fb64
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-09-21 00:06:46 +0530

    Create question3.cpp

---
+++ Assignment 1/question3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<setprecision(20)<<d<<"\n"<<setprecision(20)<<e;
+    
+    return 0;
+}
