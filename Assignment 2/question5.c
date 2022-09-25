785f08c3bcb8b0315ef7aa6767a7de708dcee4fa
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-09-25 21:49:04 +0530

    Update question5.c

--- Assignment 2/question5.c
+++ Assignment 2/question5.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-void update(int *a,int *b) {
-    
-    int s,d;
-    s=(*a + *b);
-    d=abs (*a - *b);
-    *a=s;
-    *b=d;
-  
-}
-
-int main() {
+int main() 
+{
     int a, b;
-    int *pa = &a, *pb = &b;
-    
-    scanf("%d %d", &a, &b);
-    update(pa, pb);
-    printf("%d\n%d", a, b);
+    scanf("%d\n%d", &a, &b);
+    int n;
+      for (int n=a;n<=b;n++)
+      {
+          
+          if(n==1){
+              printf("one\n");
+          }
+          else if(n==2){
+              printf("two\n");
+          }
+          else if(n==3){
+              printf("three\n");
+          }
+          else if(n==4){
+              printf("four\n");
+          }
+          else if(n==5){
+              printf("five\n");
+          }
+          else if(n==6){
+              printf("six\n");
+          }
+          else if(n==7){
+              printf("seven\n");
+          }
+          else if(n==8){
+              printf("eight\n");
+          }
+          else if(n==9){
+              printf("nine\n");
+          }
+          else{
+              if(n%2==0)
+                printf("even\n");
+              else
+                printf("odd\n");
+          }
+      }
 
     return 0;
 }
