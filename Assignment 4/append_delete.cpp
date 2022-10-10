0b19db91bba598f171cee3e22680e7f69755f23a
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-10 17:32:21 +0530

    Create append_delete.cpp

---
+++ Assignment 4/append_delete.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'appendAndDelete' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. STRING t
+ *  3. INTEGER k
+ */
+
+string appendAndDelete(string s, string t, int k) { 
+    int a=0;
+    while(a < s.size() && a < t.size()){
+        if(s[a]!=t[a])
+          break;
+          a++;
+    }
+    if(s.size()-a+t.size()-a<=k && (s.size()-a+t.size()-a)%2==k%2){
+        return "Yes";
+    }
+    else if(s.size()+t.size()<=k){
+        return "Yes";
+    }
+    else  {
+    return "No";
+    }
+    
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string t;
+    getline(cin, t);
+
+    string k_temp;
+    getline(cin, k_temp);
+
+    int k = stoi(ltrim(rtrim(k_temp)));
+
+    string result = appendAndDelete(s, t, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
