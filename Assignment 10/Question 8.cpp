1fa3a71965dc6f23967383af8fb92a9a4aa67810
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2023-01-30 23:37:40 +0530

    Create Question 8.cpp

---
+++ Assignment 10/Question 8.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>result;
+        stack<TreeNode*>S;
+        if(root)S.push(root);
+
+        while(!S.empty()){
+            TreeNode* temp=S.top();
+            S.pop();
+            result.push_back(temp->val);
+            if(temp->left)S.push(temp->left);
+            if(temp->right)S.push(temp->right);
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
+
+
+Question 145 Leetcode
